73adbaef4a2bfe5c790e9a7f3b3d8b3e0adedc01
DiegoTiscareno08 <dh1720807@gmail.com>
2022-08-26 16:10:23 -0500

    Se agrega el folder clases y los archivos main y basics extensio py

---
+++ clases/basic_operations.py
@@ -0,0 +1,12 @@
+def suma(n1,n2):
+    r=n1+n2
+    print("La suma es: ",r)
+def resta(n1,n2):
+    r=n1-n2
+    print("La resta es: ",r)
+def multiplicacion(n1,n2):
+    r=n1*n2
+    print ("La Multiplicacion es: ",r)
+def division(n1,n2):
+    r=n1/n2
+    print("La division es: ",r)
---
+++ clases/main.py
@@ -0,0 +1,9 @@
+from basic_operations import suma,resta,multiplicacion,division
+n1 = int(input("Introduce el numero 1"))
+n2= int(input("Introduce el numero 2"))
+
+suma(n1,n2)
+resta(n1,n2)
+multiplicacion(n1,n2)
+division(n1,n2)
+
